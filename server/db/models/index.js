65d0c1f449c1b801fd52370e983e7c9a65d4534f
blydiet <lydietoussaint123@gmail.com>
2019-06-28 11:56:35 -0400

    (feature-route)- we added a route for our location

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,1 +1,17 @@
+const db = require('../db')
 
+// register models
+// require('./models')
+const User = require('./users')
+const Tag = require('./tags')
+
+// User.hasMany(Order)
+// Order.belongsTo(User)
+Tag.belongsTo(User)
+User.hasMany(Tag)
+
+module.exports = {
+  db,
+  User,
+  Tag
+}
