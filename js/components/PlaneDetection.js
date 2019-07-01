b03f8add4529089a70fb20b8c6e82e9fd0037820
blydiet <lydietoussaint123@gmail.com>
2019-07-01 16:50:14 -0400

    (feature-DataBase)- we are trying to get  information to a database

--- js/components/PlaneDetection.js
+++ js/components/PlaneDetection.js
@@ -1,7 +1,8 @@
 'use strict';
 
 import React, { Component } from 'react';
-
+import { connect } from 'react-redux';
+import { getNearbyGraffiti } from '../store/graffiti';
 import { StyleSheet } from 'react-native';
 
 import {
@@ -10,22 +11,49 @@ import {
   ViroBox,
   ViroMaterials,
   ViroARPlaneSelector,
-  ViroImage
+  ViroImage,
 } from 'react-viro';
+// import console = require('console');
 
 ViroMaterials.createMaterials({
   grid: {
-    diffuseTexture: require('../res/guadalupe_360.jpg')
-  }
+    diffuseTexture: require('../res/guadalupe_360.jpg'),
+  },
 });
 
 class PlaneDetection extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
-      text: `talia's world`
+      deviceLat: 0,
+      deviceLong: 0,
+      initialLoading: false,
     };
+  }
+  async componentDidMount() {
+    await navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState(
+          {
+            deviceLat: position.coords.latitude,
+            deviceLong: position.coords.longitude,
+            error: null,
+          },
+          () => {
+            this.props.getNearbyGraffiti(
+              this.state.deviceLat,
+              this.state.deviceLong
+            );
+          }
+        );
+      },
+      error => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+    console.log(this.state.deviceLat);
+
+    this.setState({ initialLoading: true });
   }
 
   _renderScene() {
@@ -40,24 +68,53 @@ class PlaneDetection extends Component {
   }
 
   render() {
-    return (
-      <ViroARScene
-        onTrackingUpdated={() => {
-          this.setState({ text: `CONGRATULATIONS LIOR <3 <3` });
-        }}
-        anchorDetectionTypes={['PlanesVertical']} //['PlanesHorizontal', 'PlanesVertical'] props on VIROARPlaneSelector: alignment="Horizontal"
-      >
-        <ViroARPlaneSelector minHeight={0.5} minWidth={0.5} alignment="Vertical">
-          <ViroImage
+    console.log(this.props.myGraffiti);
+    if (this.state.initialLoading && this.props.myGraffiti[0]) {
+      return (
+        <ViroARScene
+          onTrackingUpdated={() => {
+            this.setState({ text: `CONGRATULATIONS LIOR <3 <3` });
+          }}
+          anchorDetectionTypes={['PlanesVertical']} //['PlanesHorizontal', 'PlanesVertical'] props on VIROARPlaneSelector: alignment="Horizontal"
+        >
+          {/* <ViroARPlaneSelector
+            minHeight={0.5}
+            minWidth={0.5}
+            alignment="Vertical"
+          >
+            <ViroImage
+              height={0.5}
+              width={0.5}
+              rotation={[-90, 0, 0]}
+              placeholderSource={require('../res/monitor.jpg')}
+              source={{ uri: this.props.myGraffiti[0].arTagUrl }}
+            />
+          </ViroARPlaneSelector> */}
+          <ViroText
+            text={this.props.myGraffiti[0].id}
+            scale={[0.5, 0.5, 0]}
+            position={[0, 0, -1]}
+          />
+          {/* <ViroImage
             height={0.5}
             width={0.5}
-            rotation={[-90,0,0]}
-            placeholderSource={require('../res/monitor.jpg')}
-            source={require('../res/graffiti.png')}
+            rotation={[-90, 0, 0]}
+            // placeholderSource={require('../res/monitor.jpg')}
+            source={{ uri: this.props.myGraffiti[0].arTagUrl }}
+          /> */}
+        </ViroARScene>
+      );
+    } else {
+      return (
+        <ViroARScene>
+          <ViroText
+            text={'Hello'}
+            scale={[0.5, 0.5, 0]}
+            position={[0, 0, -1]}
           />
-        </ViroARPlaneSelector>
-      </ViroARScene>
-    );
+        </ViroARScene>
+      );
+    }
   }
 }
 
@@ -67,8 +124,19 @@ var styles = StyleSheet.create({
     fontSize: 30,
     color: '#ee82ee',
     textAlignVertical: 'center',
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+  },
 });
 
-module.exports = PlaneDetection;
+const mapStateToProps = state => ({
+  myGraffiti: state.graffiti.nearByTags,
+});
+
+const mapDispatchToProps = dispatch => ({
+  getNearbyGraffiti: (lat, long) => dispatch(getNearbyGraffiti(lat, long)),
+});
+
+module.exports = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PlaneDetection);
