fc0258f1f5a38108c72a99d57552bb65c652dc73
Wei Ji <monokuroai@gmail.com>
2019-07-01 19:29:04 -0400

    created reducer for location/get current location action

--- js/store/index.js
+++ js/store/index.js
@@ -4,8 +4,9 @@ import thunkMiddleware from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 // import user from './users';
 import graffiti from './graffiti';
+import location from './location';
 
-const reducer = combineReducers({ graffiti });
+const reducer = combineReducers({ graffiti, location });
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
